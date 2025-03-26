e8c12ce2083ba7c2613bed2fc2ef83ea8669f880
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-03-26 21:22:15 +0200

    Created Db context

--- Expenso.StartUp/Expenso.StartUp/Program.cs
+++ Expenso.StartUp/Expenso.StartUp/Program.cs
@@ -1,7 +1,23 @@
+using Expenso.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new Exception("Connection string not found!");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
